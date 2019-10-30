badb7c9f51e982f60a8c60b1831c685f9db254b4
Felix Neumann <13457430+Themistoflix@users.noreply.github.com>
2019-10-30 14:53:45 +0100

    basic functions of NP

--- Nanoparticle.py
+++ Nanoparticle.py
@@ -1,19 +1,16 @@
-
+from ase.cluster.cubic import FaceCenteredCubic
+import numpy as np
 
 class BoundingBox:
-    def __init__(self, w, h, l, x, y, z):
+    def __init__(self, w, h, l, position):
         self.xExtent = w
         self.yExtent = l
         self.zExtent = h
 
-        self.x = x
-        self.y = y
-        self.z = z
+        self.position = position
 
-    def translate(self, dx, dy, dz):
-        self.x = self.x + dx
-        self.y = self.y + dy
-        self.z = self.z + dz
+    def translate(self, displacement):
+        self.position = self.position + displacement
 
     def rotate90Clockwise(self, aroundXAxis, aroundYAxis, aroundZAxis):
         def swap(a, b):
@@ -26,85 +23,48 @@ class BoundingBox:
         #anchor point when rotating.
         if(aroundXAxis):
             swap(self.yExtent, self.zExtent)
-            self.y = self.y - self.yExtent
+            self.position[1] = self.position[1] - self.yExtent
         if(aroundYAxis):
             swap(self.xExtent, self.zExtent)
-            self.z = self.z - self.zExtent
+            self.position[2] = self.position[2] - self.zExtent
         if(aroundZAxis):
             swap(self.xExtent, self.yExtent)
-            self.x = self.x + self.xExtent
+            self.position[0] = self.position[0] + self.xExtent
 
+class Nanoparticle(FaceCenteredCubic):
+    def __init__(self, surfaces, layers, latticeConstant, atomicSymbols):
+        super().init(atomicSymbols[0], surfaces, layers, latticeConstant)
 
-class FCCLattice:
-    #Use a righthanded cartesian coordinate system with only positive coordinates
-    #width  is associated with the x component
-    #lenght is associated with the y component
-    #height is associated with the z component
-    def __init__(self, width, length, height, latticeConstant):
-        self.width  = width
-        self.length = length
-        self.height = height
-        self.latticeConstant = latticeConstant
-
-    def getIndexFromPosition(self, x, y, z):
-        def indexInXYPlane(self, x, y):
-            if(x % 2 == 0):
-                return (y/2)*self.width + (y/2)*(self.width -1) + (x/2)
-            else:
-                return (y + 1)/2*self.width + (y - 1)*(self.width -1) + (x - 1)/2
-
-        latticePointsPerLayer = self.width*self.length + (self.width - 1)*(self.length - 1)
-        index = z*latticePointsPerLayer + indexInXYPlane(x, y)
-
-        return index
-
-    def getPositionFromIndex(self, index):
-        def positionFromPlaneIndex(self, planeIndex):
-            #a 'full block' is a row with length and the following row with lenght w -1
-            fullBlocksAbove = int(index/(w + w-1))
-            #shift the index into the first block
-            index = index - fullBlocksAbove*(w + w-1)
-
-            #check in which row the index is
-            if(index > self.width -1):
-                y = 1
-            else:
-                y = 0
-
-            if(y == 0): #upper row, with lenght=self.width
-                x = index*2
-            else: #lower row, with lenght=self.width -1
-                index = index - self.width +1
-                x = index*2 -1
-
-            return x, y
-
-
-        latticePointsPerLayer = self.width*self.length + (self.width - 1)*(self.length - 1)
-        z = int(index/latticePointsPerLayer)
-
-        planeIndex = index - z*latticePointsPerLayer
-        x, y = positionFromPlaneIndex(planeIndex)
-
-        return x, y, z
-
-class Nanoparticle:
-    def __init__(self, lattice):
-        self.lattice = lattice
-
-        self.boundingBox        = BoundingBox()
-        self.indices            = []
-        self.positionsOnLattice = []
-        self.atomNumbers        = []
+        self.boundingBox = findBoundingBox()
 
     def findBoundingBox(self):
+        positions = self.get_positions()
 
-    def generate(self, particleNumber):
+        minima = [1e10, 1e10, 1e10]
+        maxima = [-1e10, -1e10, -1e10]
 
-    def align(self, otherNanoparticle):
+        for position in position:
+            for coordinate in range(3):
+                if(minima[coordinate] > position[coordinate]):
+                    minima[coordinate] = position[coordinate]
+                if(maxima[coordinate] < position[coordinate]):
+                    maxima[coordinate] = position[coordinate]
 
-    def translate(self, dx, dy, dz):
+        w = maxima[0] - minima[0]
+        l = maxima[1] - minima[1]
+        h = maxima[2] - minima[2]
 
-    def rotate90Clockwise(self, aroundXAxis, self.aroundYAxis, self.aroundZAxis):
+        return BoundingBox(w, h, l, minima[0], minima[1], minima[2])
 
-    def getAtoms(self)
+    def translate(self, displacement):
+        super.translate(displacement)
+        self.boundingBox.translate(displacement)
+
+
+    def alignParticles(self, otherNanoparticle):
+        #TODO Align also rotation and not only orientation
+        otherAnchorPoint = otherNanoparticle.boundingBox.position
+        ownAnchorPoint = self.boundingBox.position
+
+        displacement = otherAnchorPoint - ownAnchorPoint
+        otherNanoparticle.translate(displacement)
